4163f3ac7cf729cc6f53e8918a1ff6df0136c25a
김세현 <tpgus758@gmail.com>
2023-09-25 22:40:05 +0900

    동적 클래스

--- app/components/Test.js
+++ app/components/Test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './Test.module.scss';
@@ -6,10 +6,12 @@ import styles from './Test.module.scss';
 const cx = classNames.bind(styles);
 
 const Test = () => {
+  const [isHover, setHover] = useState();
+
   return (
     <div className={cx('container')}>
       <div className={cx('wrapper')}>
-        <button className={cx('button')}>버튼</button>
+        <button className={cx(['button', { isHover }])}>버튼</button>
       </div>
     </div>
   );
