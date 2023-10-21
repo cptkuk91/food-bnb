8389d04ed686a91b2671001efbb8bbb9567fa11f
cptkuk91 <cptkuk91@gmail.com>
2023-10-22 01:02:48 +0900

    [추가] 애니메이션 효과

--- app/components/navbar/modal/Modal.tsx
+++ app/components/navbar/modal/Modal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Modal.module.scss';
+import { motion } from 'framer-motion';
 
 const cx = classNames.bind(styles);
 
@@ -25,17 +26,25 @@ const Modal: React.FC<ModalProps> = ({ isOpen, close }) => {
     { language: 'English', region: 'World' },
   ];
 
-  // !== 내부를 통해서 확인한 결과 작동은 하지만, 현재 z-index문제로 추정 해결 불가능 상태
-  // 외부 클릭했을 때 창 닫히기
   const handleCloseOnOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) close();
+  };
+
+  const modalVariants = {
+    hidden: { y: '100vh' },
+    visible: { y: '0', transition: { type: 'spring', stiffness: 50 } },
   };
 
   return (
     isOpen && (
       <div className={cx('container')} onClick={handleCloseOnOverlayClick}>
         <div className={cx('overlay')} onClick={close}></div>
-        <div className={cx('modalContent')}>
+        <motion.div
+          className={cx('modalContent')}
+          variants={modalVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden">
           <button className={cx('button')} onClick={close}>
             X
           </button>
@@ -54,7 +63,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, close }) => {
               </div>
             ))}
           </div>
-        </div>
+        </motion.div>
       </div>
     )
   );
